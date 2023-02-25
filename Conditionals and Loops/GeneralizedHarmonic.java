c00c7d2210217f379cf4f7e998ca1ea23083cef6
Aramis Kelkelyan <84933358+Ammo59@users.noreply.github.com>
2023-02-24 16:50:43 -0800

    Update and rename GeneralizedHarmonic to GeneralizedHarmonic.java

--- Conditionals and Loops/GeneralizedHarmonic.java
+++ Conditionals and Loops/GeneralizedHarmonic.java
@@ -1,3 +1,5 @@
+// This file takes in two user inputs as arguments and outputs the result from a harmonic series
+
 public class GeneralizedHarmonic {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
@@ -11,3 +13,4 @@ public class GeneralizedHarmonic {
         System.out.println(H);
     }
 }
+
